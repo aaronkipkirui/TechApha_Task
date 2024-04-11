e09b39d6ecc38fc9e6de223547c1144215659efa
Kemoi <aaronkipkirui015@gmail.com>
2024-04-11 21:21:59 +0300

    Java program to remove leading zeros in a String

---
+++ src/RemoveLeadingZero.java
@@ -0,0 +1,26 @@
+
+public class RemoveLeadingZero {
+    public static void main(String[] args) {
+//        initializing input and output variables
+        String input = "0000012789";
+        String output = removeLeadingZeros(input);
+//      print input and output after removing zeros
+        System.out.println("Original string: " + input);
+        System.out.println("Output after removing zeros: " + output);
+
+    }
+//  removeLeadingZeros method
+    public static String removeLeadingZeros(String input) {
+//        find index of first non-zero character
+        int i = 0;
+        while (i < input.length() && input.charAt(i) == '0'){
+            i++;
+        }
+//        if characters are zero return "0"
+        if (i == input.length()){
+            return "0";
+        }
+//        return substring starting from the first non-zero character
+        return input.substring(i);
+    }
+}
